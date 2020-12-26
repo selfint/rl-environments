196865dd48a00fccc63a3804a20886568861faff
Tom Selfin <selfint@gmail.com>
2020-12-26 17:11:23 +0200

    use jump environment display trait instead of local function

--- examples/jump_environment.rs
+++ examples/jump_environment.rs
@@ -1,60 +1,26 @@
 use std::{thread, time};
 
-use rl_environments::jump_environment::{JumpEnvironment, JumpEnvironmentTile};
+use rl_environments::jump_environment::JumpEnvironment;
 
 /// always jumps
 fn simple_jumper(_env: &JumpEnvironment) -> bool {
     true
 }
 
-/// displays tile
-fn display_tile(tile: &JumpEnvironmentTile) -> &str {
-    match tile {
-        JumpEnvironmentTile::Empty => " ",
-        JumpEnvironmentTile::Ground => "#",
-        JumpEnvironmentTile::Player => "O",
-        JumpEnvironmentTile::Wall => "|",
-    }
-}
-
-/// displays jump environment state
-fn display_state(env: &JumpEnvironment) {
-    let mut rows = vec![];
-    let state = env.state();
-    for col in state.iter() {
-        for (i, tile) in col.iter().enumerate() {
-            if rows.len() < i + 1 {
-                let row = vec![tile];
-                rows.push(row);
-            } else {
-                rows[i].push(tile);
-            }
-        }
-    }
-
-    // clear console and reset cursor
-    print!("\x1B[2J\x1B[1;1H");
-
-    for row in rows.iter().rev() {
-        for &tile in row {
-            let tile_str = display_tile(tile);
-            print!("{}", tile_str);
-        }
-        println!();
-    }
-}
-
 fn main() {
-    let mut env = JumpEnvironment::new(10);
+    let mut env = JumpEnvironment::new(11);
     let mut rewards = 0;
-    for _ in 0..100 {
-        display_state(&env);
+    while !env.done {
+        // clear console and reset cursor
+        print!("\x1B[2J\x1B[1;1H");
+
+        println!("{}", &env);
         let jump = simple_jumper(&env);
         if jump {
             env.jump();
         }
         rewards += env.update();
-        println!("Rewards={}", rewards);
-        thread::sleep(time::Duration::from_millis(100));
+        println!("Rewards={} dead={}", rewards, env.done);
+        thread::sleep(time::Duration::from_millis(300));
     }
 }
